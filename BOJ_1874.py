ab5edbb4ce45ced42279201bd7f230a036a604ec
lee-ji-an <leeja0407@naver.com>
2022-07-15 23:50:40 +0900

    add datastructure(1874)

--- BOJ_1874.py
+++ BOJ_1874.py
@@ -2,24 +2,33 @@ import sys
 n = int(sys.stdin.readline())
 l = []
 stack = [0 for _ in range(n)]
+ans = []
 value = 1
 ptr = -1
+boolean = True
 for i in range(n):
     l.append(int(sys.stdin.readline()))
 for i in range(n):
     if value <= l[i]:
         for j in range(l[i]-value+1):
-            print("+")
+            ans.append("+")
             ptr = ptr + 1
             stack[ptr] = value
-        value = value + (l[i] - value) + 1
-        print("-")
+            value = value + 1
+        ans.append("-")
         ptr = ptr - 1
     elif ptr != -1:
-        if stack[ptr] == value:
-            print("-")
+        if stack[ptr] == l[i]:
+            ans.append("-")
             ptr = ptr - 1
+        else:
+            print("NO")
+            boolean = False
+            break
     else:
         print("NO")
+        boolean = False
         break
-    print("---")
+if boolean:
+    for j in range(len(ans)):
+        print(ans[j])
