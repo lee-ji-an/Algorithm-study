eccc17c249f7c9054ecfbf19e06983ba18ad1f0a
lee-ji-an <leeja0407@naver.com>
2022-08-17 23:37:50 +0900

    Add DynamicProgramming(1495)

---
+++ DynamicProgramming/BOJ_1495.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+n, s, m = map(int, input().split())
+v = list(map(int, input().split()))
+dp = [[0]*(m+1) for _ in range(n+1)]
+dp[0][s] = 1
+for i in range(1, n+1):
+    s = v[i-1]
+    for j in range(m+1):
+        if dp[i-1][j] == 1:
+            if j+s <= m:
+                dp[i][j+s] = 1
+            if j-s >= 0:
+                dp[i][j-s] = 1
+flag = True
+for i in range(m, -1, -1):
+    if dp[n][i] == 1:
+        print(i)
+        flag = False
+        break
+if flag:
+    print(-1)
