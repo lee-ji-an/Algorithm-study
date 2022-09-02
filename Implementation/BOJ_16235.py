5656402d32b47ebac7be20444da8f9f98bb9f5a7
lee-ji-an <leeja0407@naver.com>
2022-09-03 03:42:52 +0900

    Modify Implementation(16235)

--- Implementation/BOJ_16235.py
+++ Implementation/BOJ_16235.py
@@ -1,3 +1,8 @@
+#Date : 2022.08.29
+#Update : 2022.09.03
+#Classification : Implementation
+#Author : leejian
+
 import sys
 input = sys.stdin.readline
 N, M, K = map(int, input().split())
@@ -7,56 +12,40 @@ earth = [[5] * N for _ in range(N)]
 # 나무 나이, 수
 tree = [[[] for _ in range(N)] for _ in range(N)]   ## 배열 만드는 것 익히기
 cnt = 0
-
+dr = [1, -1, 0, 0, 1, -1, 1, -1]
+dc = [0, 0, 1, -1, 1, 1, -1, -1]
 def spring():
     global plus
     for r in range(N):
         for c in range(N):
             if len(tree[r][c]) <= 0:
                 continue
-            flag = False
             for l in range(len(tree[r][c])-1, -1, -1):
                 energy = earth[r][c] - tree[r][c][l]
                 if energy < 0:
-                    flag = True
+                    summer(r, c, l)
+                    tree[r][c] = tree[r][c][l + 1:]
                     break
-                earth[r][c] = energy
-                tree[r][c][l] += 1
-                if tree[r][c][l] % 5 == 0:  # 죽기 전에 넣어버린다
-                    plus.append((r, c))
-            if flag:
-                summer(r, c, l)
-                tree[r][c] = tree[r][c][l+1 : ]
+                else:
+                    earth[r][c] = energy
+                    tree[r][c][l] += 1
+                    if tree[r][c][l] % 5 == 0:  # 죽기 전에 넣어버린다
+                        plus.append((r, c))
+
 
 def summer(row, col, l):
-    for _ in range(0, l + 1):
-        earth[row][col] += tree[row][col][l] // 2
+    for x in range(0, l + 1):   ## 고생했던 부분
+        earth[row][col] += tree[row][col][x] // 2
 
 
 def fall():
     for r, c in plus:
-        left, right, top, bottom = False, False, False, False   ##주의
-        # print("row, col : ",r, c)
-        if c+1 < N:
-            tree[r][c+1].append(1)
-            right = True
-        if c-1 >= 0:
-            tree[r][c-1].append(1)
-            left = True
-        if r+1 < N:
-            tree[r+1][c].append(1)
-            bottom = True
-        if r-1 >= 0:
-            tree[r-1][c].append(1)
-            top = True
-        if right and top:
-            tree[r-1][c+1].append(1)
-        if right and bottom:
-            tree[r+1][c+1].append(1)
-        if left and top:
-            tree[r-1][c-1].append(1)
-        if left and bottom:
-            tree[r+1][c-1].append(1)
+        for x in range(8):
+            mr = r + dr[x]
+            mc = c + dc[x]
+            if 0 <= mr < N and 0 <= mc < N:
+                tree[mr][mc].append(1)
+
 
 def winter():
     for r in range(N):
@@ -75,10 +64,6 @@ for i in range(K):
     spring()
     fall()
     winter()
-# for i in range(N):
-#     print(tree[i])
-# for i in range(N):
-#     print(earth[i])
 
 for r in range(N):
     for c in range(N):
