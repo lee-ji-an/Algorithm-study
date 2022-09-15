fbceb561063463e9f42d92363bcc73a202ab84ec
lee-ji-an <leeja0407@naver.com>
2022-09-15 18:21:29 +0900

    [Algorithm2] Update Union Find

--- Algorithm2/[Union Find] [코드]-1/Percolate.py
+++ Algorithm2/[Union Find] [코드]-1/Percolate.py
@@ -1,43 +1,65 @@
 import random
-ids = []
-size = []   # size[i]: size of tree rooted at i
+import statistics
+import math
 
-def root(r1, c1):
-    while r1*5+c1 != ids[r1][c1]:  = ids[r1][c1]
+def root(i):
+    while i != ids[i]: i = ids[i]
     return i
 
 def connected(p, q):
     return root(p) == root(q)
 
-def union(r1, c1, r2, c2):
-    id1r, id1c = root(ids[r1][c1])
-    id2r, id2c = root(ids[r2][c2])
-    if id1r == id2r and id1c == id2c: return
-    if size[id1r][id1c] <= size[id2r][id2c]:
-        ids[id1r][id1c] = ids[]
+def union(p, q):
+    id1, id2 = root(p), root(q)
+    if id1 == id2: return
+    if size[id1] <= size[id2]:
+        ids[id1] = id2
         size[id2] += size[id1]
     else:
         ids[id2] = id1
         size[id1] += size[id2]
-N = int(input())
-ids = [[0 for y in range(N)] for x in range(N)]
-size = [[1 for y in range(N)] for x in range(N)]
-size.append([N+1, N+1])
-ids.append([N*N, N*N+1])
-for i in range(N):
-    for j in range(N):
-        ids[i][j] = i*5+j
-for i in range(N):
-    ids[0][i] = N*N
-    ids[N-1][i] = N*N+1
-print(size)
-print(ids)
-while True:
-    r = random.randrange(0, 5)
-    c = random.randrange(0, 5)
-    union(ids[r][c], ids[r-1][c])
-    union(ids[r][c], ids[r+1][c])
 
-# for idx in range(N):
-#     ids.append(idx)
-#     size.append(1)
+def simulate(n, t):
+    global ids, size
+    prob = []
+    ids = [0] * (n * n + 2)
+    size = [1] * (n * n + 2)
+    visited = [0] * (n * n + 2)
+    for x in range(t):
+        cnt = 0
+        for j in range(0, n*n+2):
+            ids[j] = j
+            size[j] = 1
+            visited[j] = 0
+        for j in range(n):
+            ids[j] = n * n
+        for j in range(n * (n - 1), n * n):
+            ids[j] = n * n + 1
+        while True:
+            num = random.randrange(0, n*n)
+            if visited[num]:
+                continue
+            visited[num] = 1
+            cnt += 1
+            if n*(n-1) <= num:
+                size[n*n+1] += 1
+            elif 0 <= num < n:
+                size[n*n] += 1
+            if num-n >= 0 and visited[num-n]:
+                union(num, num-n)
+            if num+n < n*n and visited[num+n]:
+                union(num, num+n)
+            if num % n != n-1 and visited[num+1]:
+                union(num, num+1)
+            if num % n != 0 and visited[num-1]:
+                union(num, num-1)
+            if connected(n*n, n*n+1):
+                break
+        prob.append(cnt/(n*n))
+    mean = statistics.mean(prob)
+    stdev = statistics.stdev(prob)
+    interval = [mean-1.96*stdev/math.sqrt(t), mean+1.96*stdev/math.sqrt(t)]
+    print("mean                    = ", format(mean, ".10f"))
+    print("stdev                   = ", format(stdev, ".10f"))
+    print("95% confidence interval = [", format(interval[0], ".10f"),format(interval[1], ".10f"), "]")
+    return mean, stdev
